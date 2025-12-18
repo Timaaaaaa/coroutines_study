435cb742a510a7e445fab42278039b653627526a
tolegenov <tolegenov@kolesa.kz>
2025-12-18 14:44:38 +0500

    sync issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
 // Root build configuration for the coroutine study project.
-buildscript {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        google()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-    }
+plugins {
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 subprojects {
@@ -15,11 +8,6 @@ subprojects {
 
     group = "com.way.coroutines"
     version = "1.0.0"
-
-    repositories {
-        mavenCentral()
-        google()
-    }
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
